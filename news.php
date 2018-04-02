479c2009275d82afbca2058391c10e159ac61587
Mihai Hutan <mihai@Alins-MacBook-Pro.local>
2018-04-02 14:47:59 +0300

    News Page

---
+++ news.php
@@ -0,0 +1,119 @@
+<?php
+  $title = "Earth.com - Take Action";
+  include "includes/header.php";
+?>
+
+<!-- News of the Day and Top Stories Row -->
+<div class="container">
+  <div class="row">
+    <!-- News of the Day Image -->
+    <div class="col-lg-8 news-of-the-day-image">
+      <div class="row">
+        <div class="col-lg-6">
+
+          <div class="highlight-ribbon background-red news-ribbon">
+            Story of the Day
+          </div>
+          <img class="news-img" src="assets/images/news/news-of-the-day.jpg" alt="News of the Day" />
+        </div>
+        <div class="col-lg-6">
+          <div class="row article-category-date">
+            <div class="col-lg-6">
+              ENVIRONMENT
+            </div>
+            <div class="col-lg-6">
+              11-22-2018
+            </div>
+          </div>
+          <h2 class="news-title">Stu Unger Rise and Fall Of A Poker Genius</h2>
+          <p class="news-excerpt">
+            Computer users and programmers have become so accustomed to using
+            Windows, even for the ...
+          </p>
+          <a href="#">Read more...</a>
+        </div>
+      </div>
+    </div>
+
+    <!-- Right sidebar with Top Stories -->
+    <div class="col-lg-4">
+      <h2 class="section-title">BREAKING NEWS</h2>
+
+      <!-- Top Story Article -->
+      <article class="top-story">
+        <div class="row">
+          <div class="col-lg-4">
+            <img src="assets/images/news/top-story-thumb.jpg" alt="Top Story" />
+          </div>
+          <div class="col-lg-8">
+            <div class="row article-category-date">
+              <div class="col-md-6 category">
+                ENVIRONMENT
+              </div>
+              <div class="col-md-6 date">
+                11-22-2018
+              </div>
+            </div>
+            <div class="row article-title">
+              <div class="col-xs-12">
+                Coventry City Guide Including Coventry Hotels
+              </div>
+            </div>
+          </div>
+        </div>
+      </article>
+
+      <!-- Top Story Article -->
+      <article class="top-story">
+        <div class="row">
+          <div class="col-lg-4">
+            <img src="assets/images/news/top-story-thumb.jpg" alt="Top Story" />
+          </div>
+          <div class="col-lg-8">
+            <div class="row article-category-date">
+              <div class="col-md-6 category">
+                NEWS
+              </div>
+              <div class="col-md-6 date">
+                11-22-2018
+              </div>
+            </div>
+            <div class="row article-title">
+              <div class="col-xs-12">
+                The Luxury Of Traveling With Yacht Charter Companies
+              </div>
+            </div>
+          </div>
+        </div>
+      </article>
+
+      <!-- Top Story Article -->
+      <article class="top-story">
+        <div class="row">
+          <div class="col-lg-4">
+            <img src="assets/images/news/top-story-thumb.jpg" alt="Top Story" />
+          </div>
+          <div class="col-lg-8">
+            <div class="row article-category-date">
+              <div class="col-md-6 category">
+                ENVIRONMENT
+              </div>
+              <div class="col-md-6 date">
+                11-22-2018
+              </div>
+            </div>
+            <div class="row article-title">
+              <div class="col-xs-12">
+                Coventry City Guide Including Coventry Hotels
+              </div>
+            </div>
+          </div>
+        </div>
+      </article>
+  </div>
+</div>
+</div>
+
+<?php
+  include "includes/footer.php";
+?>
