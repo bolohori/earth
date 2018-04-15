23df7c87c7b7abfedc197cf3b9461d450e2320e4
Mitzu <mihai.hutan92@gmail.com>
2018-04-16 01:13:22 +0300

    Earthpedia page

--- src/earthpedia.php
+++ src/earthpedia.php
@@ -185,6 +185,293 @@
   </div>
 </section>
 
+<!-- PlantSnap mobile app -->
+<section class="plantsnap-mobile-app">
+  <div class="container-big">
+    <div class="row">
+      <div class="col-lg-5 left">
+        <h1>Identify plants <br />on the go</h1>
+        <p>Plantsnap is a mobile app that helps you identify plants, flowers, trees, cacti and mushrooms. Simply snap a photo on the plant, and PlantSnap tell you what it is! </p>
+        <a href="#">Find out more</a>
+      </div>
+
+      <div class="col-lg-2 middle">
+        <img src="assets/images/plantsnap-phone.png" alt="Plantsnap">
+      </div>
+
+      <div class="col-lg-5 right">
+        <div>
+          <img src="assets/images/app-store-badge.png" alt="App Store">
+        </div>
+        <div>
+          <img src="assets/images/google-play-badge.png" alt="Google Play">
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!-- Animal videos -->
+<section class="animal-videos">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8 left">
+        <h2>ANIMAL VIDEOS</h2>
+        <div class="row video-section">
+          <div class="col-sm-6">
+            <video width="350" controls>
+              <source src="assets/videos/sample.mp4" type=video/mp4 />
+            </video>
+            <div class="date">
+              11-22-2018
+            </div>
+            <h3>Coventry City Guide Including Coventry Hotels</h3>
+          </div>
+          <div class="col-sm-6">
+            <div class="row">
+              <div class="col-lg-6">
+                <video width="170" controls="">
+                  <source src="assets/videos/sample.mp4" type="video/mp4">
+                </video>
+              </div>
+              <div class="col-lg-6">
+                <div class="row article-category-date">
+                  <div class="col-md-6 category">
+                    ENVIRONMENT
+                  </div>
+                  <div class="col-md-6 date">
+                    11-22-2018
+                  </div>
+                </div>
+                <div class="row article-title">
+                  <div class="col-xs-12">
+                    Coventry City Guide Including Coventry Hotels
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-lg-6">
+                <video width="170" controls="">
+                  <source src="assets/videos/sample.mp4" type="video/mp4">
+                </video>
+              </div>
+              <div class="col-lg-6">
+                <div class="row article-category-date">
+                  <div class="col-md-6 category">
+                    ENVIRONMENT
+                  </div>
+                  <div class="col-md-6 date">
+                    11-22-2018
+                  </div>
+                </div>
+                <div class="row article-title">
+                  <div class="col-xs-12">
+                    Coventry City Guide Including Coventry Hotels
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
+      </div>
+
+      <h2>PLANTS VIDEOS</h2>
+      <div class="row video-section">
+        <div class="col-sm-6">
+          <video width="350" controls>
+            <source src="assets/videos/sample.mp4" type=video/mp4 />
+          </video>
+          <div class="date">
+            11-22-2018
+          </div>
+          <h3>Coventry City Guide Including Coventry Hotels</h3>
+        </div>
+        <div class="col-sm-6">
+          <div class="row">
+            <div class="col-lg-6">
+              <video width="170" controls="">
+                <source src="assets/videos/sample.mp4" type="video/mp4">
+              </video>
+            </div>
+            <div class="col-lg-6">
+              <div class="row article-category-date">
+                <div class="col-md-6 category">
+                  ENVIRONMENT
+                </div>
+                <div class="col-md-6 date">
+                  11-22-2018
+                </div>
+              </div>
+              <div class="row article-title">
+                <div class="col-xs-12">
+                  Coventry City Guide Including Coventry Hotels
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-lg-6">
+              <video width="170" controls="">
+                <source src="assets/videos/sample.mp4" type="video/mp4">
+              </video>
+            </div>
+            <div class="col-lg-6">
+              <div class="row article-category-date">
+                <div class="col-md-6 category">
+                  ENVIRONMENT
+                </div>
+                <div class="col-md-6 date">
+                  11-22-2018
+                </div>
+              </div>
+              <div class="row article-title">
+                <div class="col-xs-12">
+                  Coventry City Guide Including Coventry Hotels
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
+    </div>
+
+    </div>
+      <div class="col-lg-4 right">
+        <h2 class="section-title">RELATED STORIES</h2>
+
+        <!-- Top Story Article -->
+        <article class="top-story">
+          <div class="row">
+            <div class="col-lg-6">
+              <video width="170" controls="">
+                <source src="assets/videos/sample.mp4" type="video/mp4">
+              </video>
+            </div>
+            <div class="col-lg-6">
+              <div class="row article-category-date">
+                <div class="col-md-6 category">
+                  ENVIRONMENT
+                </div>
+                <div class="col-md-6 date">
+                  11-22-2018
+                </div>
+              </div>
+              <div class="row article-title">
+                <div class="col-xs-12">
+                  Coventry City Guide Including Coventry Hotels
+                </div>
+              </div>
+            </div>
+          </div>
+        </article>
+
+        <!-- Top Story Article -->
+        <article class="top-story">
+          <div class="row">
+            <div class="col-lg-6">
+              <video width="170" controls="">
+                <source src="assets/videos/sample.mp4" type="video/mp4">
+              </video>
+            </div>
+            <div class="col-lg-6">
+              <div class="row article-category-date">
+                <div class="col-md-6 category">
+                  ENVIRONMENT
+                </div>
+                <div class="col-md-6 date">
+                  11-22-2018
+                </div>
+              </div>
+              <div class="row article-title">
+                <div class="col-xs-12">
+                  Coventry City Guide Including Coventry Hotels
+                </div>
+              </div>
+            </div>
+          </div>
+        </article>
+
+        <!-- Top Story Article -->
+        <article class="top-story">
+          <div class="row">
+            <div class="col-lg-6">
+              <video width="170" controls="">
+                <source src="assets/videos/sample.mp4" type="video/mp4">
+              </video>
+            </div>
+            <div class="col-lg-6">
+              <div class="row article-category-date">
+                <div class="col-md-6 category">
+                  ENVIRONMENT
+                </div>
+                <div class="col-md-6 date">
+                  11-22-2018
+                </div>
+              </div>
+              <div class="row article-title">
+                <div class="col-xs-12">
+                  Coventry City Guide Including Coventry Hotels
+                </div>
+              </div>
+            </div>
+          </div>
+        </article>
+        <!-- Top Story Article -->
+        <article class="top-story">
+          <div class="row">
+            <div class="col-lg-6">
+              <video width="170" controls="">
+                <source src="assets/videos/sample.mp4" type="video/mp4">
+              </video>
+            </div>
+            <div class="col-lg-6">
+              <div class="row article-category-date">
+                <div class="col-md-6 category">
+                  ENVIRONMENT
+                </div>
+                <div class="col-md-6 date">
+                  11-22-2018
+                </div>
+              </div>
+              <div class="row article-title">
+                <div class="col-xs-12">
+                  Coventry City Guide Including Coventry Hotels
+                </div>
+              </div>
+            </div>
+          </div>
+        </article>
+        <!-- Top Story Article -->
+        <article class="top-story">
+          <div class="row">
+            <div class="col-lg-6">
+              <video width="170" controls="">
+                <source src="assets/videos/sample.mp4" type="video/mp4">
+              </video>
+            </div>
+            <div class="col-lg-6">
+              <div class="row article-category-date">
+                <div class="col-md-6 category">
+                  ENVIRONMENT
+                </div>
+                <div class="col-md-6 date">
+                  11-22-2018
+                </div>
+              </div>
+              <div class="row article-title">
+                <div class="col-xs-12">
+                  Coventry City Guide Including Coventry Hotels
+                </div>
+              </div>
+            </div>
+          </div>
+        </article>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
 
 <!-- Explore Plants and Animals -->
 <div class="container-big friends-of-animals">
