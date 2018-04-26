9c61dd3a208ccc6dbd03f21ec1e59bf27c33aa6c
Horia <horia.bologan@dma-tech.com>
2018-04-26 15:39:39 +0300

    Contact page M T D ready

--- src/contact.php
+++ src/contact.php
@@ -3,54 +3,62 @@
   include "./includes/head.php";
   include "includes/header.php";
 ?>
-
-<div class="container contact-page-title-container">
-  <h1 class="contact-page-title">Contact Us</h1>
-</div>
-
-<div class="container-big contact-us-container">
-  <div class="row">
-    <div class="col-md-8">
-      <form class="contact-form" action="" method="post">
-        <div class="row">
-          <div class="col-md-6 contact-form-column">
-            <div class="form-group">
-              <label for="name">Name</label>
-              <input type="text" class="form-control" id="name" placeholder="">
-            </div>
-            <div class="form-group">
-              <label for="email">Name</label>
-              <input type="email" class="form-control" id="email" placeholder="">
-            </div>
-            <div class="form-group">
-              <label for="subject">Subject</label>
-              <input type="text" class="form-control" id="subject" placeholder="">
-            </div>
-            <button type="submit" class="btn btn-primary contact-btn">SEND MESSAGE</button>
-          </div>
-          <div class="col-md-6">
-            <label for="message">Message</label>
-            <textarea class="form-control" id="message" rows="8"></textarea>
-          </div>
+<section id="contact">
+    <div class="container-fluid bg-white">
+        <div class="col-12">
+            <h1 class="title text-uppercase m-0">Contact Us</h1>
         </div>
-      </form>
     </div>
-    <div class="col-md-4 address-column">
-      <h2 class="contact-subtitle">Office</h2>
-      <address>
-        Darwinberg <br />
-        95 Brennan Junction Suite <br />
-        441-330-8325 <br />
-        hello@earth.com
-      </address>
-      <h2 class="contact-subtitle">Organizations</h2>
-      <address>
-        partnerships@earth.com
-      </address>
+    <div class="container">
+        <div class="row wrapper">
+            <div class="col-lg-8">
+                <form id="contact-form" action="" method="post">
+                    <div class="row">
+                        <div class="col-md-6 contact-col">
+                            <div class="form-group">
+                                <label for="name">Name</label>
+                                <input type="text" class="form-control" id="name" placeholder="">
+                            </div>
+                            <div class="form-group">
+                                <label for="email">Email</label>
+                                <input type="email" class="form-control" id="email" placeholder="">
+                            </div>
+                            <div class="form-group">
+                                <label for="subject">Subject</label>
+                                <input type="text" class="form-control" id="subject" placeholder="">
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="message">Message</label>
+                                <textarea class="form-control" id="message"
+                                          rows="8"></textarea>
+                            </div>
+                        </div>
+                        <div class="col-12 btn-col">
+                            <button type="submit"
+                                    class="btn btn-primary btn-contact">SEND
+                                MESSAGE</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="col-lg-4 address-col d-flex">
+                <address>
+                    <h2 class="subtitle">Office</h2>
+                    Darwinberg <br />
+                    95 Brennan Junction Suite <br />
+                    <a href="tel:4413308325">441-330-8325</a> <br />
+                    <a href="mailto:hello@earth.com">hello@earth.com</a>
+                </address>
+                <address>
+                    <h2 class="subtitle">Organizations</h2>
+                    <a href="mailto:partnerships@earth.com">partnerships@earth.com</a>
+                </address>
+            </div>
+        </div>
     </div>
-  </div>
-</div>
-
+</section>
 <!-- News Coming Your Way -->
 <?php include "includes/newsletter.php" ?>
 
