fb8412766e81eb896d5df54a76780bfee8ddd1ab
Horia <horia.bologan@dma-tech.com>
2018-05-11 17:59:54 +0300

    Privacy policy page done

---
+++ src/includes/internal-header.php
@@ -0,0 +1,70 @@
+<section id="<?php echo $navId; ?>" class="main-navigation">
+	<nav class="navbar navbar-expand-lg navbar-dark main-nav mb-0 justify-content-start justify-content-md-between">
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+			<img src="./assets/images/icons/burger_icon.png" alt=""> <span class=""></span><!--navbar-toggler-icon-->
+		</button>
+		<!--<a href="#" class="secondary-menu-toggler"><i class="fas fa-bars"></i></a>-->
+		<a class="navbar-brand" href="index.php">
+			<img class="w-auto" src="assets/images/logos/logo-white.png" alt="earth.com logo">
+		</a>
+		<span class="page-title text-uppercase">
+			<?php echo $title; ?>
+		</span>
+		<div class="collapse navbar-collapse collapse" id="navbarsExample04">
+			<ul class="navbar-nav mx-auto">
+				<li class="nav-item">
+					<a class="nav-link" href="breaking-news.php">Breaking News</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="lifestyle.php">Lifestyle</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="images.php">Environment</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="science.php">Science</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="plants.php">Plants</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="animals.php">Animals</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="voices.php">Voices</a>
+				</li>
+			</ul>
+		</div>
+		<div class="actions d-flex ml-auto">
+			<div class="navbar-search my-auto">
+				<i class="fas fa-search"></i>
+				<!--<input type="search" placeholder="Search">-->
+			</div>
+			<a href="#" class="btn btn-subscribe align-items-center d-md-inline-flex d-none">SUBSCRIBE</a>
+		</div>
+	</nav>
+</section>
+
+<!-- Header Add -->
+<section id="main-add" class="main-add">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-12 text-center">
+				HEADER AD
+			</div>
+		</div>
+	</div>
+</section>
+<!-- END Header Add -->
+
+<!-- BreadCrumbs -->
+<section id="breadcrumbs">
+	<div class="container">
+		<div class="breadcrumbs">
+			<div class="col-xs-12">
+				<a href="index.php"><i class="fas fa-globe"></i></a> / <a href="news.php">News </a>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- END BreadCrumbs -->
---
+++ src/privacy-policy.php
@@ -0,0 +1,149 @@
+<?php
+  /* Insert page's title */
+  $title = "Home";
+
+  /* Insert page's menu section ID for custom navigation elements */
+  $navId = 'home-menu';
+
+  /* Logo image url*/
+  $logoUrl = "assets/images/logos/logo-white.png";
+  include "includes/head.php";
+  include "includes/header.php";
+?>
+<section id="privacy-policy" class="legal-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="headline mb-5 text-uppercase">Privacy Policy</h1>
+            </div>
+            <div class="col-md-12 col-lg-8">
+                <!--featured article-->
+                <article class="card legal">
+                    <p class="text">This privacy statement covers the site www.Earth.com.net. Because this Web site wants to demonstrate its commitment to our users’ privacy, it has agreed to disclose its information practices.</p>
+                    <hr class="small-paragraph-divider m-0 invisible">
+                    <ul class="legal-list">
+                        <li class="mb-4">What personally identiﬁable information Earth.com, Inc. collects.</li>
+                        <li class="mb-4">What personally identiﬁable information third parties collect through the Web site.</li>
+                        <li class="mb-4">What organization collects the information.</li>
+                        <li class="mb-4">How Earth.com, Inc. uses the information.</li>
+                        <li class="mb-4">With whom Earth.com, Inc. may share user information.</li>
+                        <li class="mb-4">What choices are available to users regarding collection, use and distribution of the information.</li>
+                        <li class="mb-4">What types of security procedures are in place to protect the loss, misuse or alteration of information under Earth.com, Inc. control.</li>
+                        <li class="mb-4">How users can correct any inaccuracies in the information.</li>
+                    </ul>
+                    <hr class="big-paragraph-divider m-0 invisible">
+
+                    <h3 class="category">Infromation Collection and Use</h3>
+
+                    <h5 class="subcategory text-uppercase">Information collection</h5>
+
+                    <h6 class="subtitle">Registration</h6>
+                    <p class="text">Earth.com, Inc. is the sole owner of the information collected on Earth.com.com. Earth.com, Inc. collects information from our users at several different points on our Web site.</p>
+                    <hr class="big-paragraph-divider m-0 invisible">
+
+                    <h6 class="subtitle">Order</h6>
+                    <p class="text">We request information from the user on our order form. A user must provide contact information (such as name, email, and shipping address) and financial information (such as credit card number, expiration date). This information is used for billing purposes and to fill customer’s orders. If we have trouble processing an order, the information is used to contact the user.</p>
+                    <hr class="big-paragraph-divider m-0 invisible">
+
+                    <h5 class="subcategory text-uppercase">Information use</h5>
+                    <h6 class="subtitle">Profile</h6>
+                    <p class="text">We store information that we collect through cookies, log files, clear gifs, and/or third parties to create a profile of our users. A profile is stored information that we keep on individual users that details their viewing preferences. Consequently, collected information is tied to the users personally identifiable information to provide offers and improve the content of the site for the user. This profile is used to tailor a user’s visit to our Web site, and to direct pertinent marketing promotions to them. We do not share your profile with other third parties.</p>
+                    <hr class="big-paragraph-divider m-0 invisible">
+
+                    <h6 class="subtitle">Cookies</h6>
+                    <p class="text">A cookie is a piece of data stored on the user’s computer tied to information about the user. Usage of a cookie is in no way linked to any personally identifiable information while on our site. We use both session ID cookies [and] persistent cookies. For the session ID cookie, once users close the browser, the cookie simply terminates. A persistent cookie is a small text file stored on the user’s hard drive for an extended period of time. Persistent cookies can be removed by following Internet browser help file directions.</p>
+                    <hr class="small-paragraph-divider m-0 invisible">
+                    <p class="text">We use cookies to enhance the Earth.com experience. By setting a cookie on our site, users would not have to log in a password more than once, thereby saving time while on our site. If users reject the cookie, they may still use our site. The only drawback to this is that the user will be limited in some areas of our site. For example, the user will not be able to participate in any of our sweepstakes, contests or monthly drawings that take place. Persistent cookies enable us to track and target the interests of our users to enhance the experience on our site. See the ‘Profile’ section.</p>
+                    <hr class="small-paragraph-divider m-0 invisible">
+                    <p class="text">Some of our business partners use cookies on our site (for example, advertisers). However, we have no access to or control over these cookies, once we have given permission for them to set cookies for advertising.</p>
+                    <hr class="small-paragraph-divider m-0 invisible">
+
+                </article>
+                <!--end of legal article-->
+            </div>
+            <!--sidebar-->
+            <section class="col-12 col-md-6 col-lg-4 article-list sidebar">
+                <h4 class="category">
+                    Top Stories
+                </h4>
+                <article class="story row no-gutters">
+                    <div class="image col-auto col-md-4 pr-2 pr-md-3">
+                        <img class="img-fluid h-100" src="./assets/images/placeholder%20images/sidebar-thumb.jpg" alt="Card image">
+                    </div>
+                    <!--image-->
+                    <div class="body align-self-stretch d-flex flex-column col col-md-8 pr-2 ">
+                        <div class="meta mb-1 d-flex flex-wrap justify-content-between">
+                            <span class="tag">Environment</span>
+                            <span class="date">21-12-2020</span>
+                        </div>
+                        <!--meta-->
+                        <h5 class="title m-0">
+                            <a href="#">Coventry City Guide Including Coventry hotels</a>
+                        </h5>
+                    </div>
+                    <!--article-body-->
+                </article>
+                <article class="story row no-gutters">
+                    <div class="image col-auto col-md-4 pr-2 pr-md-3">
+                        <img class="img-fluid h-100" src="./assets/images/placeholder%20images/sidebar-thumb.jpg" alt="Card image">
+                    </div>
+                    <!--image-->
+                    <div class="body align-self-stretch d-flex flex-column col col-md-8 pr-2 ">
+                        <div class="meta mb-1 d-flex flex-wrap justify-content-between">
+                            <span class="tag">Environment</span>
+                            <span class="date">21-12-2020</span>
+                        </div>
+                        <!--meta-->
+                        <h5 class="title m-0">
+                            <a href="#">Coventry City Guide Including Coventry hotels</a>
+                        </h5>
+                    </div>
+                    <!--article-body-->
+                </article>
+                <article class="story row no-gutters">
+                    <div class="image col-auto col-md-4 pr-2 pr-md-3">
+                        <img class="img-fluid h-100" src="./assets/images/placeholder%20images/sidebar-thumb.jpg" alt="Card image">
+                    </div>
+                    <!--image-->
+                    <div class="body align-self-stretch d-flex flex-column col col-md-8 pr-2 ">
+                        <div class="meta mb-1 d-flex flex-wrap justify-content-between">
+                            <span class="tag">Environment</span>
+                            <span class="date">21-12-2020</span>
+                        </div>
+                        <!--meta-->
+                        <h5 class="title m-0">
+                            <a href="#">Coventry City Guide Including Coventry hotels</a>
+                        </h5>
+                    </div>
+                    <!--article-body-->
+                </article>
+                <article class="story row no-gutters">
+                    <div class="image col-auto col-md-4 pr-2 pr-md-3">
+                        <img class="img-fluid h-100" src="./assets/images/placeholder%20images/sidebar-thumb.jpg" alt="Card image">
+                    </div>
+                    <!--image-->
+                    <div class="body align-self-stretch d-flex flex-column col col-md-8 pr-2 ">
+                        <div class="meta mb-1 d-flex flex-wrap justify-content-between">
+                            <span class="tag">Environment</span>
+                            <span class="date">21-12-2020</span>
+                        </div>
+                        <!--meta-->
+                        <h5 class="title m-0">
+                            <a href="#">Coventry City Guide Including Coventry hotels</a>
+                        </h5>
+                    </div>
+                    <!--article-body-->
+                </article>
+                <div class="ad mx-auto">
+                    <img class="img-fluid h-100 d-none d-lg-block" src="./assets/images/placeholder%20images/sidebar-ad.png" alt="">
+                </div>
+            </section>
+            <!--sidebar-->
+            <!--end of sidebar-->
+        </div>
+    </div>
+</section>
+<?php
+    include "includes/newsletter.php";
+    include "includes/footer.php";
+?>
